2f30d2655a68c62ecc482a3359b1ffac55611cc6
adgrafik <webmaster@adgrafik.at>
2016-02-17 13:59:59 +0100

    [TASK] Changed required version to 6.2.

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'typo3' => '7.4.0-7.99.99',
+			'typo3' => '6.2.0-7.99.99',
 			'adx_less' => '1.1.2-1.2.99',
 			'gridelements' => '4.0.0-4.99.99',
 		),
